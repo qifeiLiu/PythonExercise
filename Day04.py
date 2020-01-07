0bd23e1962eac8624922857571aadfd2e020f6dd
Qifei liu(3P) <qifei.liu@citrix.com>
2020-01-07 09:57:39 +0800

    Day04-1

--- Day04.py
+++ Day04.py
@@ -12,27 +12,41 @@
 # 5月：第20天
 # 31+28+31+30+20= 140
 
-datetime=input("请输入日期(格式:2010-10-1):")
+datetime=input("请输入日期(格式:2010-10-1)不按格式输入可能会出错:")
 year = int(datetime.split('-')[0])
 month= int(datetime.split('-')[1])
 day = int(datetime.split('-')[2])
-days_in_month_tuple=(31,28,31,30,31,30,31,31,30,31,30,31)
-sum = 0
-
+# days_in_month_tuple=(31,28,31,30,31,30,31,31,30,31,30,31)
+# sum = 0
+#没有对输入进行判断，所以在输入不正确的时候程序可能会出错
 def leap_year(year):
     if ((year % 4 == 0) and (year % 100 != 0)): # 不是百年的情况 只要满足整除4即可
         return True
     if (year % 100 == 0) and (year % 400 == 0): # 百年的情况 要同时满足 整除400
         return True
+    print("False")
     return False
 
-if (leap_year(year)== False) or (month <=2):
-    for i in range(0,month-1):
-        sum += days_in_month_tuple[i]
+# if (leap_year(year)== False) or (month <=2):# 不是闰年 或者月份比小于等于2 的
+#     for i in range(0,month-1):
+#         sum += days_in_month_tuple[i]
+#     sum += day
+# else:
+#     for i in range(0,month-1):
+#         sum += days_in_month_tuple[i]
+#     sum +=day
+#     sum +=1 # 闰年加一天
+# print("第{}天".format(sum))
+
+#方法2：
+days_in_month_tuple=(0,31,59,120,151,181,212,243,273,304,334)# 如果日期在第一个月，则没有前一个月需要加
+                                                             # 如果日期是2月1 日，则需要加上一月的31天
+if (leap_year(year)== False) or (month <=2):# 不是闰年 或者月份比小于等于2 的
+    sum = days_in_month_tuple[month-1]
     sum += day
 else:
-    for i in range(0,month-1):
-        sum += days_in_month_tuple[i]
+    sum = days_in_month_tuple[month-1]
     sum +=day
     sum +=1 # 闰年加一天
 print("第{}天".format(sum))
+#为什么方法二比方法一更好呢?
