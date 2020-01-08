a75c093dcbee7c80f0631d5f8a1d55708cac62f1
qifei liu <qifei.liu@foxmail.com>
2020-01-08 08:51:44 +0800

    Day06

---
+++ Day06.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/8 8:34
+# @File    : Day06.py
+# @Software: PyCharm
+
+#题目: 用 * 输出字母 C
+print("hello world! python")
+print('* '* 10)
+for i in range(5):
+    print('**')
+print('* '* 10)
+print('*\n'*6)
